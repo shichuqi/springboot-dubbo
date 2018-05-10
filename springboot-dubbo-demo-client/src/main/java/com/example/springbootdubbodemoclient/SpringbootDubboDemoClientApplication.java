06aa1682b66e96599f6f426ff3394d900dc845f4
shicq <shichuqi@lidig.com>
2018-05-10 11:42:11 +0800

    flowable

--- springboot-dubbo-demo-client/src/main/java/com/example/springbootdubbodemoclient/SpringbootDubboDemoClientApplication.java
+++ springboot-dubbo-demo-client/src/main/java/com/example/springbootdubbodemoclient/SpringbootDubboDemoClientApplication.java
@@ -3,7 +3,9 @@ package com.example.springbootdubbodemoclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
+        org.flowable.spring.boot.SecurityAutoConfiguration.class})
 public class SpringbootDubboDemoClientApplication {
 
     public static void main(String[] args) {
